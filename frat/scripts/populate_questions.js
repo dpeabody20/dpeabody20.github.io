49bf1eeccba64d1eaf41dc11ae013162579f59ff
dpeabody20 <48960191+dpeabody20@users.noreply.github.com>
2024-11-05 12:09:24 -0700

    Update populate_questions.js

--- frat/scripts/populate_questions.js
+++ frat/scripts/populate_questions.js
@@ -1,17 +1,17 @@
-import questions from './questions.json' assert {type: 'json'};
+//import questions from './questions.json' assert {type: 'json'};
 
 
 function populate_questions() {
     
 
-//    fetch('./questions.json')
-//        .then((response) => response.json())
-//        .then((json) => console.log(json));
+    fetch('./questions.json')
+        .then((response) => response.json())
+        .then((json) => console.log(json));
     
     console.log("Questions Populating");
 
-    let questions_div = document.getElementById("questions");
+    //let questions_div = document.getElementById("questions");
 
-    console.log(questions);
+    //console.log(questions);
 
 }
